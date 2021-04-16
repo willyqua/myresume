b29613df772080f27a415958f4efa6fef1c3f11e
willy quasevi <willy.quasevi@gmail.com>
2021-04-16 22:39:37 +0200

    first commit

---
+++ src/script.js
@@ -0,0 +1,6 @@
+// TYPED
+
+var typed = new Typed('.typed', {
+    strings: ["Actuellement titulaire d'un BTS en développement WEB obtenu en Octobre 2020, je suis à la recherche de ma prochaine aventure professionnelle, dans le domaine des applications Web.Je suis à l’écoute de toute opportunité dans la métropole lilloise tout en restant ouvert de nouvelle proposition géographique.Si vous êtes intéressé(e)s, n’hésitez pas à me contacter en message privé sur LinkedIn ou par mail."],
+    typeSpeed: 10,
+});
